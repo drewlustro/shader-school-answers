b9f761262421918f99a862bb926f081cff33271a
Samuel Hurel <samuel.hurel@gmail.com>
2018-01-10 17:03:26 +0100

    Use `bgra` to swap colors

--- 09-frag-3/fragment.glsl
+++ 09-frag-3/fragment.glsl
@@ -7,8 +7,6 @@ void main() {
   vec2 coord = gl_FragCoord.xy / screenSize;
 
   //TODO: Swap red and blue color channels of image
-  vec4 color = texture2D(texture, coord);
-  vec4 swapped = vec4(color.b, color.g, color.r, 1.0);
-  gl_FragColor = swapped;
+  gl_FragColor = texture2D(texture, coord).bgra;
   // gl_FragColor = texture2D(texture, coord);
 }
